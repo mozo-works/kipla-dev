222d65af5ada41f27ffcd0309497bb7590cf5de1
yeongtaek, Hong <mozodev@users.noreply.github.com>
2022-02-23 11:44:16 +0900

    gsheet 함수를 추가합니다.

---
+++ netlify/functions/gsheet/index.js
@@ -0,0 +1,79 @@
+/**
+ * Google spreadsheet
+ *
+ * POST /.netlify/functions/gsheet
+ *
+ * @see env-vars
+ * GOOGLE_SERVICE_ACCOUNT_EMAIL=
+ * GOOGLE_PRIVATE_KEY=
+ * GOOGLE_SPREADSHEET_ID_FROM_URL=
+ *
+ * from https://github.com/sw-yx/netlify-google-spreadsheet-demo
+ */
+
+ const { GoogleSpreadsheet } = require('google-spreadsheet');
+
+ if (!process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL)
+   throw new Error('no GOOGLE_SERVICE_ACCOUNT_EMAIL env var set');
+ if (!process.env.GOOGLE_PRIVATE_KEY)
+   throw new Error('no GOOGLE_PRIVATE_KEY env var set');
+ if (!process.env.GOOGLE_SPREADSHEET_ID_FROM_URL)
+   throw new Error('no GOOGLE_SPREADSHEET_ID_FROM_URL env var set');
+ 
+ exports.handler = async (event, context) => {
+   const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID_FROM_URL);
+   await doc.useServiceAccountAuth({
+     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+     private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
+   });
+   await doc.loadInfo();
+ 
+   const path = event.path.replace(/\.netlify\/functions\/[^/]+/, '');
+   const segments = path.split('/').filter((e) => e);
+   let sheet = doc.sheetsByIndex[0];
+   if (segments.length === 1 && segments[0] in doc.sheetsByTitle) {
+     sheet = doc.sheetsByTitle[segments[0]];
+   }
+ 
+   try {
+     switch (event.httpMethod) {
+       case 'GET':
+         const rows = await sheet.getRows();
+         const serializedRows = rows.map(serializeRow);
+         return {
+           statusCode: 200,
+           body: JSON.stringify(serializedRows),
+           headers: {
+             'Access-Control-Allow-Credentials': true,
+             'Access-Control-Allow-Origin': '*',
+             'Content-Type': 'application/json',
+           },
+         };
+       default:
+         return {
+           statusCode: 500,
+           body: 'unrecognized HTTP Method, must be one of GET/POST/PUT/DELETE'
+         };
+     }
+   }
+   catch (err) {
+     console.error('error ocurred in processing ', event);
+     console.error(err);
+     return {
+       statusCode: 500,
+       body: err.toString()
+     };
+   }
+ 
+   /**
+    * utils
+    */
+   function serializeRow(row) {
+     let temp = {};
+     sheet.headerValues.map((header) => {
+       temp[header] = row[header];
+     });
+     return temp;
+   }
+ };
+ 
